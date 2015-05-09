084dcf2a56f31cc23c31f8f91025a54788b06623
wxx5433 <wxx5433@gmail.com>
2015-05-08 22:52:12 -0400

    fit malloc bug in graph.cpp

--- src/graph.cpp
+++ src/graph.cpp
@@ -2,8 +2,10 @@
 #include <string.h>
 
 void build_start(graph *graph, int *scratch) {
-  graph->outgoing_starts = (int*)malloc(sizeof(int) * graph->num_nodes + 1);
-  memcpy(graph->outgoing_starts, scratch, sizeof(int) * graph->num_nodes);
+  graph->outgoing_starts = (int*)malloc(sizeof(int) * (graph->num_nodes + 1));
+  for (int i = 0; i < graph->num_nodes; ++i) {
+    graph->outgoing_starts[i] = scratch[i];
+  }
   graph->outgoing_starts[graph->num_nodes] = graph->num_edges;
 }
 
@@ -11,7 +13,9 @@ void build_edges(graph *graph, int *scratch) {
   int num_nodes = graph->num_nodes;
 
   graph->outgoing_edges = (int*)malloc(sizeof(int) * graph->num_edges);
-  memcpy(graph->outgoing_edges, scratch + num_nodes, sizeof(int) * graph->num_edges);
+  for (int i = 0; i < graph->num_edges; ++i) {
+    graph->outgoing_edges[i] = scratch[num_nodes + i];
+  }
 }
 
 void build_virtual_graph(const int *outgoing_starts, const int *outgoing_edges, 
